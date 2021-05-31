4e57d2068ae60a8399dec2af6b631b754e31c7d7
shvanshu <shivanshu@gmail.com>
2021-05-31 21:10:50 +0530

    update module

--- python tutorials/projects/march2021_project/project/server/index.js
+++ python tutorials/projects/march2021_project/project/server/index.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -6,6 +5,7 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
+require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
